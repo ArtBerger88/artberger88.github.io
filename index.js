823defa333a6c4e95e8c4b2a49311c817dc7a145
artyz <artberg88@gmail.com>
2024-10-19 11:44:19 -0400

    uncommemted form and form css added js email submit functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //this is my java script file // 
+window.onload = function() {
+    emailjs.init(
+        "Sdo8-Z9nfVOIvI3fs");
+    
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_wdhkm95', 'template_dp1nv7d', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
