617e7e31308249ea964337447d4386fc39e79384
artyz <artberg88@gmail.com>
2024-10-19 12:40:39 -0400

    added required status to inputs and error messages also

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ window.onload = function() {
     
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+        const name = document.querySelector('input[name="Contact-Name"]');
+        const email = document.querySelector('input[name="Contact-Email"]');
+        const message = document.querySelector('textarea[name="Contact-Message"]');
+        const errorMessage = document.getElementById('message-error');
+        if (!name.value) {
+            alert('Please input a name.');
+            return;
+        }
+        if (!email.value) {
+            alert('Please input an email.');
+            return;
+        }
+        if (!message.value.trim()) {
+            errorMessage.textContent = 'Please input a message.';
+            return;
+        }
         // these IDs from the previous steps
         emailjs.sendForm('service_wdhkm95', 'template_01jpnfu', this)
             .then(() => {
                 console.log('SUCCESS!');
+                    this.reset();
             }, (error) => {
                 console.log('FAILED...', error);
             });
